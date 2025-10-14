580d53e5d18ac5244a35819836c17eee5a6bbbb7
MarianoAstudillo <astudillo.mariano.0075@gmail.com>
2025-10-14 12:01:10 +0000

    Added write file function

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,7 +5,18 @@ from functions.config import *
 
 def write_file(working_directory, file_path, content):
   try:
-   
+    full_path = os.path.join(working_directory, file_path)
+    dirname_path = os.path.dirname(os.path.abspath(full_path))
+
+    if not os.path.exists(dirname_path):
+      os.makedirs(dirname_path, mode=0o777, exist_ok=True)
+    
+    if not full_path.startswith(working_directory):
+      raise Exception(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
+    
+    with open(full_path, "w") as f:
+      f.write(content)
+      return (f'Successfully wrote to "{file_path}" ({len(content)} characters written)')
 
   except Exception as e:
     print(f'Error: {e}')
--- tests.py
+++ tests.py
@@ -1,23 +1,36 @@
 # from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 # result = get_files_content("calculator", "lorem.txt")
 # if result:
 #   print(result)
 
 
-result = get_file_content("calculator", "main.py")
+result = write_file("calculator", "lorem2.txt", "wait, this isn't lorem ipsum")
 if result:
   print(result)
 
-result = get_file_content("calculator", "pkg/calculator.py")
+result = write_file("calculator", "test/morelorem.txt", "lorem ipsum dolor sit amet")
 if result:
   print(result)
 
-result = get_file_content("calculator", "/bin/cat")
+result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
 if result:
   print(result)
 
-result = get_file_content("calculator", "pkg/does_not_exist.py")
-if result:
-  print(result)
+
+# result = get_file_content("calculator", "main.py")
+# if result:
+#   print(result)
+
+# result = get_file_content("calculator", "pkg/calculator.py")
+# if result:
+#   print(result)
+
+# result = get_file_content("calculator", "/bin/cat")
+# if result:
+#   print(result)
+
+# result = get_file_content("calculator", "pkg/does_not_exist.py")
+# if result:
+#   print(result)
